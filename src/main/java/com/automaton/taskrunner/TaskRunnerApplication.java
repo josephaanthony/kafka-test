61fa876d0d2643966dc1505cf21bc8b7cdbf46e6
joseph <josephaanthony@gmail.com>
2020-06-18 17:21:10 -0400

    Initial Java App

---
+++ src/main/java/com/automaton/taskrunner/TaskRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.automaton.taskrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class TaskRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automaton/taskrunner/TaskRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automaton.taskrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
